6c92225e89d895d02f6099374442c355d25ab5ad
Matthew Malone <mama4294@colorado.edu>
2022-08-06 15:35:06 -0600

    starts settings page

--- components/navbar.js
+++ components/navbar.js
@@ -1,29 +1,39 @@
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import MenuIcon from "@mui/icons-material/Menu";
+import SettingsIcon from "@mui/icons-material/Settings";
+import Settings from "../components/settings";
 
 const Navbar = () => {
+  const [openSettings, setOpenSettings] = useState(false);
+  const handleOpenSettings = () => setOpenSettings(true);
+  const handleCloseSettings = () => setOpenSettings(false);
+
   return (
-    <AppBar position="fixed">
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Process
-        </Typography>
-        <Button color="inherit">Save</Button>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={handleOpenSettings}
+          >
+            <SettingsIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Process
+          </Typography>
+          <Button color="inherit">Save</Button>
+        </Toolbar>
+      </AppBar>
+      <Settings open={openSettings} handleClose={handleCloseSettings} />
+    </>
   );
 };
 
