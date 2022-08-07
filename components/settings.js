bc2e1cc420148dd6ac2b9a5ceb2a7a55f9124162
Matthew Malone <mama4294@colorado.edu>
2022-08-07 12:03:40 -0600

    adds sidebar functionality to settings page

--- components/settings.js
+++ components/settings.js
@@ -13,11 +13,21 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
+import ClearAllIcon from "@mui/icons-material/ClearAll";
+import BloodtypeIcon from "@mui/icons-material/Bloodtype";
 import MailIcon from "@mui/icons-material/Mail";
+import PersonIcon from "@mui/icons-material/Person";
 import { blueGrey } from "@mui/material/colors";
 import { useWindowWide } from "../hooks/windowWidth";
 
 const Settings = ({ open, handleClose }) => {
+  const menuItems = [
+    { id: 0, title: "Batches", icon: "ClearAllIcon" },
+    { id: 1, title: "Resources", icon: "BloodtypeIcon" },
+    { id: 2, title: "Profile", icon: "PersonIcon" },
+  ];
+  const [selectedMenu, setSelectedMenu] = useState(menuItems[0].title);
+
   const windowWidth = useWindowWide();
   const drawerWidth =
     windowWidth < 700 ? windowWidth * 0.95 : windowWidth * 0.8;
@@ -36,24 +46,56 @@ const Settings = ({ open, handleClose }) => {
         variant="temporary"
       >
         <Box sx={{ display: "flex" }}>
-          <AppBar position="static" sx={{ zIndex: 1201 }}>
+          <AppBar
+            position="fixed"
+            sx={{ zIndex: 1201, width: drawerWidth, left: 0 }}
+          >
             <Toolbar>
               <>
                 <IconButton onClick={handleClose}>
                   <CloseIcon sx={{ color: blueGrey[50] }} />
                 </IconButton>
-                <h1>Settings</h1>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  Settings
+                </Typography>
               </>
             </Toolbar>
           </AppBar>
-          <Sidebar width={sideBarWidth} />
+          <Sidebar
+            menuItems={menuItems}
+            width={sideBarWidth}
+            setSelectedMenu={setSelectedMenu}
+          />
+          <Box component="main" sx={{ flexGrow: 1, px: 3 }}>
+            <Toolbar />
+            {selectedMenu === "Batches" && <BatchesMenu />}
+            {selectedMenu === "Resources" && <ResourcesMenu />}
+            {selectedMenu === "Profile" && <ProfileMenu />}
+          </Box>
         </Box>
       </Drawer>
     </div>
   );
 };
 
-const Sidebar = ({ width }) => {
+const Sidebar = ({ menuItems, width, setSelectedMenu }) => {
+  const handleChange = (title) => {
+    setSelectedMenu(title);
+  };
+
+  const getIcon = (icon) => {
+    switch (icon) {
+      case "PersonIcon":
+        return <PersonIcon />;
+      case "ClearAllIcon":
+        return <ClearAllIcon />;
+      case "BloodtypeIcon":
+        return <BloodtypeIcon />;
+      default:
+        return <MailIcon />;
+    }
+  };
+
   return (
     <Drawer
       variant="permanent"
@@ -69,26 +111,11 @@ const Sidebar = ({ width }) => {
       <Toolbar />
       <Box sx={{ overflow: "auto" }}>
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
+          {menuItems.map((item, index) => (
+            <ListItem key={item.title} disablePadding>
+              <ListItemButton onClick={() => handleChange(item.title)}>
+                <ListItemIcon>{getIcon(item.icon)}</ListItemIcon>
+                <ListItemText primary={item.title} />
               </ListItemButton>
             </ListItem>
           ))}
@@ -96,6 +123,18 @@ const Sidebar = ({ width }) => {
       </Box>
     </Drawer>
   );
+};
+
+const BatchesMenu = () => {
+  return <h6>Batches</h6>;
+};
+
+const ResourcesMenu = () => {
+  return <h6>Resources</h6>;
+};
+
+const ProfileMenu = () => {
+  return <h6>Profile</h6>;
 };
 
 export default Settings;
