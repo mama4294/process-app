352b91c1ff80d812b5f755d8243a0579c6cfd869
Matthew Malone <mama4294@colorado.edu>
2022-08-19 07:50:42 -0600

    adds view selector to action bar

--- components/actionBar.js
+++ components/actionBar.js
@@ -1,35 +1,34 @@
-import Button from "@mui/material/Button";
+import React, { useState } from "react";
 import { EquipmentContext } from "../contexts/equipmentContext";
+import Button from "@mui/material/Button";
 import { useContext } from "react";
 import styles from "../styles/ActionBar.module.css";
 import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
-import EditIcon from "@mui/icons-material/Edit";
 import Tooltip from "@mui/material/Tooltip";
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import CachedIcon from "@mui/icons-material/Cached";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import AlignHorizontalLeftIcon from "@mui/icons-material/AlignHorizontalLeft";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ShowChartIcon from "@mui/icons-material/ShowChart";
 
-const ActionBar = ({ handleNew, handleEdit }) => {
-  const {
-    selectionIds,
-    deleteEquipment,
-    solveEquipmentOccupancy,
-    moveUp,
-    moveDown,
-  } = useContext(EquipmentContext);
+const ActionBar = ({ handleNew }) => {
+  const { solveEquipmentOccupancy } = useContext(EquipmentContext);
 
   return (
     <div className={styles.actionBar}>
+      <ViewSelector />
       <Tooltip title="Add Equipment">
         <IconButton onClick={handleNew}>
-          <AddIcon color="action" />
+          <AddIcon />
         </IconButton>
       </Tooltip>
       <Tooltip title="Recalculate schedule">
         <IconButton onClick={solveEquipmentOccupancy}>
-          <CachedIcon color="action" />
+          <CachedIcon />
         </IconButton>
       </Tooltip>
     </div>
@@ -37,3 +36,56 @@ const ActionBar = ({ handleNew, handleEdit }) => {
 };
 
 export default ActionBar;
+
+const ViewSelector = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Tooltip title="Change View">
+        <Button
+          id="demo-customized-button"
+          aria-controls={open ? "demo-customized-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          // variant="contained"
+          disableElevation
+          onClick={handleClick}
+          startIcon={<AlignHorizontalLeftIcon />}
+          endIcon={<KeyboardArrowDownIcon />}
+        >
+          Chart
+        </Button>
+      </Tooltip>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <AlignHorizontalLeftIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Chart</ListItemText>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <ShowChartIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Resources</ListItemText>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+};
